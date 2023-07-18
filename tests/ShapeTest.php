ef7afe751e6458165e784403dbc537b43f1bd9db
Gopalakrishna B <gopalakrishna.b@optisolbusiness.com>
2023-07-18 14:51:37 +0530

    test case changes

--- tests/ShapeTest.php
+++ tests/ShapeTest.php
@@ -11,58 +11,51 @@ class ShapeTest extends WebTestCase
     {
         $this->assertTrue(true);
     }
+
     public function testTriangleSuccess(): void
     {
         $client = static::createClient();
-
         // Request a specific page
         $crawler = $client->request('GET', '/api/v1/triangle/3/4/5');
-
         // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
     }
+
     public function testTriangleSuccessDecimal(): void
     {
         $client = static::createClient();
-
         // Request a specific page
         $crawler = $client->request('GET', '/api/v1/triangle/3.4/4.5/5');
-
         // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
     }
+
     public function testCircleSuccess(): void
     {
         $client = static::createClient();
-
         // Request a specific page
         $crawler = $client->request('GET', '/api/v1/circle/2');
-
         // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
     }
-    public function testCircleSuccessDecimal(): void
-    {
-        $client = static::createClient();
 
+    public function testCircleSuccessDecimal(): void    {
+        $client = static::createClient();
         // Request a specific page
         $crawler = $client->request('GET', '/api/v1/circle/2.5');
-
         // Validate a successful response and some content
         $this->assertResponseIsSuccessful();
     }
-    public function testCircleFail(): void
-    {
-        $client = static::createClient();
 
+    public function testCircleFail(): void    {
+        $client = static::createClient();
         // Request a specific page
         $crawler = $client->request('GET', '/api/v1/circle');
         $this->assertFalse(false);
     }
-    public function testTriangleFail(): void
-    {
-        $client = static::createClient();
 
+    public function testTriangleFail(): void    {
+        $client = static::createClient();
         // Request a specific page
         $crawler = $client->request('GET', '/api/v1/triangle');
         $this->assertFalse(false);
